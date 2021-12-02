93fd11d037d558ab2545570ad711204e087d45c6
Nehemiah Campbell <nehemiah.campbell@endjin.com>
2021-12-02 13:14:47 +0000

    day2 part 2

--- 2021/day2.py
+++ 2021/day2.py
@@ -13,3 +13,22 @@ with open("data/input2.txt", "r") as f:
 
     assert forward * depth == 1989265
     print("Part 1: Pass")
+
+# %%
+with open("data/input2.txt", "r") as f:
+    aim = forward = depth = 0
+    for lines in f.readlines():
+        dirs, size = lines.split()
+        size = int(size)
+        if dirs == "down":
+            aim += size
+        elif dirs == "up":
+            aim -= size
+        elif dirs == "forward":
+            forward += size
+            depth += size * aim
+
+    assert forward * depth == 2089174012
+    print("Part 2: Pass")
+
+# %%
