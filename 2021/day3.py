decfa5670a42fac8ce54422c5480929bd14170ba
Nehemiah Campbell <nehemiah.campbell@endjin.com>
2021-12-03 17:04:50 +0000

    part 1 done

--- 2021/day3.py
+++ 2021/day3.py
@@ -10,5 +10,11 @@ def col(grid, column):
 
 
 # %%
+most_common_bit_string  = ''.join(["1" if col(data, row).count('1') > len(data) // 2 else "0"
+    for row in range(len(data[0]))])
 
+least_common_bit_string = ''.join(["0" if x == "1" else "1" for x in most_common_bit_string])
+
+# %%
+int(most_common_bit_string, 2) * int(least_common_bit_string, 2)
 # %%
