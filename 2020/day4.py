eda845e8605b3244be7ab1ffc31a090125812bc0
Nehemiah Campbell <nehemiahcampbell@hotmail.co.uk>
2021-12-05 19:50:51 +0000

    refactor 2020 day 4 part 1

--- 2020/day4.py
+++ 2020/day4.py
@@ -1,37 +1,37 @@
-# %%
-with open("data/.txt") as f:
-    data = f.read()
-# %%
-passport = [line for line in data.split("\n\n")]
-print(passport)
-# %%
+from advent_of_code.core import *
 
+passport = data("2020/data/input_4.txt", sep="\n\n", test=False)
+
+test_input = """ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in"""
+
+test_input = data(test_input, sep="\n\n")
 
 codes = ("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")  # "cid")
 
 # %%
-number_not_valid = 0
-for docs in passport:
-    not_valid = False
-    for c in codes:
-        if c not in docs:
-            not_valid = True
-    if not_valid:
-        number_not_valid += 1
-
-len(passport) - number_not_valid
+assert quantify(test_input, lambda col: all(c in col for c in codes)) == 2
+assert quantify(passport, lambda col: all(c in col for c in codes)) == 260
 
 
-"""
-* byr (Birth Year) - four digits; at least 1920 and at most 2002.
-* iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-* eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
-* hgt (Height) - a number followed by either cm or in:
-* If cm, the number must be at least 150 and at most 193.
-* If in, the number must be at least 59 and at most 76.
-* hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-* ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-* pid (Passport ID) - a nine-digit number, including leading zeroes.
-* cid (Country ID) - ignored, missing or not.
-
-"""
+# * byr (Birth Year) - four digits; at least 1920 and at most 2002.
+# * iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+# * eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+# * hgt (Height) - a number followed by either cm or in:
+# * If cm, the number must be at least 150 and at most 193.
+# * If in, the number must be at least 59 and at most 76.
+# * hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+# * ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+# * pid (Passport ID) - a nine-digit number, including leading zeroes.
+# * cid (Country ID) - ignored, missing or not.
