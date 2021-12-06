f0bf61c5f8639aafb8e0825e7d967fb51e04d73d
Nehemiah Campbell <nehemiah.campbell@endjin.com>
2021-12-06 18:27:08 +0000

    2021 day 6 part 2

--- 2021/day6.py
+++ 2021/day6.py
@@ -1,34 +1,30 @@
 #%%
 from advent_of_code.core import *
+from collections import deque
 
 #%%
 _test_input = "3,4,3,1,2"
 test_input = data(_test_input, sep=",", parser=int)
 
-
-def tick(n: int) -> int:
-    "apply lantern rule"
-    if n > 0:
-        return n - 1
-    return 6
-
-
-def simulate(fish: list, days: int) -> int:
-    cache = {}
-    for _ in range(days):
-        zeros = fish.count(0)
-        fish = list(map(tick, fish))
-        fish += [8] * zeros
-    return length(fish)
-
-
-# assert simulate(test_input, 18) == 26
-# assert simulate(test_input, 80) == 5934
-
-_input = data("2021/data/input6.txt", sep=",", parser=int, test=False)
 #%%
-print(simulate(_input, 80))
-# assert simulate(test_input, 256) == 26984457539
-# simulate(test_input, 256)
+def simulate(fish: list, days: int) -> int:
+    q = deque([fish.count(x) for x in range(9)])
+    for _ in range(days):
+        zeros = q.popleft()
+        q.append(zeros)
+        q[6] += zeros
+    return sum(q)
+
+
+#%%
+print(simulate(test_input, 18))
+assert simulate(test_input, 18) == 26
+assert simulate(test_input, 80) == 5934
+
+_input = data("data/input6.txt", sep=",", parser=int, test=False)
+
+assert simulate(_input, 80) == 360268
+assert simulate(test_input, 256) == 26984457539
+simulate(_input, 256)
 
 # %%
