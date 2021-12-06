97258f91517992529ea97254d4ac134302023e1f
Nehemiah Campbell <nehemiahcampbell@hotmail.co.uk>
2021-12-06 08:17:24 +0000

    2020 day 4 part 2 in progress

--- 2020/day4.py
+++ 2020/day4.py
@@ -1,8 +1,8 @@
 from advent_of_code.core import *
 
-passport = data("2020/data/input_4.txt", sep="\n\n", test=False)
+parser = lambda s: s.replace("\n", " ").split(" ")
 
-test_input = """ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+_test_input = """ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
 byr:1937 iyr:2017 cid:147 hgt:183cm
 
 iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
@@ -16,11 +16,13 @@ hgt:179cm
 hcl:#cfa07d eyr:2025 pid:166559648
 iyr:2011 ecl:brn hgt:59in"""
 
-test_input = data(test_input, sep="\n\n")
+test_input = data(_test_input, sep="\n\n")
 
-codes = ("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")  # "cid")
+codes = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
 
 # %%
+
+passport = data("2020/data/input_4.txt", sep="\n\n", test=False)
 assert quantify(test_input, lambda col: all(c in col for c in codes)) == 2
 assert quantify(passport, lambda col: all(c in col for c in codes)) == 260
 
@@ -35,3 +37,26 @@ assert quantify(passport, lambda col: all(c in col for c in codes)) == 260
 # * ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
 # * pid (Passport ID) - a nine-digit number, including leading zeroes.
 # * cid (Country ID) - ignored, missing or not.
+
+test_input2 = mapt(parser, test_input)
+
+str_to_dict = lambda col: dict([tuple(s.split(":")) for s in col])
+test_input2 = [str_to_dict(passport) for passport in test_input2]
+
+def isvalid(v) -> dict:
+    rules = {
+    'byr' : "1920" <= v <= "2002",
+    'iyr' : "2020" <= v <= "2020",
+    'eyr' : "2020" <= v <= "2030",
+    'hgt' : ((v.endswith('cm') and "150" <= v <= "193") or
+             (v.endswith('in') and "59" <= v <= "76")),
+    'hcl' : first(v) == "#" and 
+            all(cl in "0123456789abcdef" for cl in v[1:]) and 
+            len(v) == 6,
+    'ecl' : v in ('amb', 'blu', 'brn', 'gry', 'hzl', 'oth'),
+    'pid' : len(v) == 9 and set(v) == set(str(x) for x in range(10)),
+    }
+    return rules
+
+def valid_passport(passport):
+    return (all(c in codes for c in passport))
