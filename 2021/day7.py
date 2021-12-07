06bd12c0bdf1a8e92f26999a11fb0fa8d8a12e48
Nehemiah Campbell <nehemiah.campbell@endjin.com>
2021-12-07 10:19:21 +0000

    2021 day 7: part 1 and 2 complete. And input updated

--- 2021/day7.py
+++ 2021/day7.py
@@ -1,3 +1,4 @@
+
 from advent_of_code.core import *
 
 _test_input = "16,1,2,0,4,2,7,1,2,14"
@@ -7,15 +8,34 @@ test_input = data(_test_input, sep=",", parser=int)
 _input = data('data/input7.txt', sep=",", parser=int, test=False) # 0 -> 1937
 
 
-def get_position(_data):
-    pos = [0 for _ in range(max(_data)+1)]
+def get_position(data, distance_fn):
+    pos = [0 for _ in range(max(data)+1)]
     for i in range(len(pos)):
-        for fuel in _data:
-            pos[i] += abs(fuel - i)
+        for fuel in (data):
+            pos[i] += distance_fn(fuel, i)
     return min(pos)
 
-print(get_position(test_input))
-assert get_position(test_input) == 37
-print(get_position(_input))
+def absolute(x: int, y: int) -> int:
+    return abs(x - y)
 
-# 345254
+def natural_sum(n: int) -> int:
+    return n * (n + 1) // 2
+
+def compose(f, g):
+    return lambda *x: f(g(*x))
+
+    
+# assert get_position(test_input, absolute) == 37
+print(get_position(test_input, absolute))
+print(get_position(_input,  absolute))
+# assert get_position(test_input) == 168
+
+print(get_position(
+    test_input, 
+    compose(natural_sum, absolute)))
+
+print(get_position(
+    _input, 
+    compose(natural_sum, absolute)))
+
+
