8c9f9c27a52ffb6f1bf44a5b5aaebaa404a6ecfd
Nehemiah Campbell <nehemiah.campbell@endjin.com>
2021-12-10 09:49:44 +0000

    2021 day 10 part 2

--- 2021/day10.py
+++ 2021/day10.py
@@ -1,3 +1,4 @@
+#%%
 from typing import Union, List
 from advent_of_code.core import data
 
@@ -12,10 +13,11 @@ _test_input = """[({(<(())[]>[[{[]{<()<>>
 <{([([[(<>()){}]>(<<{{
 <{([{{}}[<[[[<>{}]]]>[]]"""
 
-brackets = {"{":"}", "[":"]", "(":")", "<":">"}
+brackets = {"{": "}", "[": "]", "(": ")", "<": ">"}
 
 test_input = data(_test_input)
-_input = data('data/input10.txt', test=False)
+_input = data("data/input10.txt", test=False)
+
 
 def balanced_paren(string: str) -> Union[str, List[str]]:
     stack = []
@@ -28,13 +30,41 @@ def balanced_paren(string: str) -> Union[str, List[str]]:
             return char
     return stack[::-1]
 
+
 print([balanced_paren(t) for t in test_input])
 
-# def get_score(data: list) -> int:
-#     points  = {")": 3, "]": 57, "}": 1197, ">": 25137}
-#     illegals = [balanced_paren(test) for test in data]
-#     return sum(illegals.count(key)*value for key,value in points.items())
-# 
-# assert get_score(test_input) == 26397
-# print(get_score(_input)) # 415953
 
+def get_score(data: list) -> int:
+    points = {")": 3, "]": 57, "}": 1197, ">": 25137}
+    illegals = tuple(
+        filter(lambda x: isinstance(x, str), [balanced_paren(test) for test in data])
+    )
+    return sum(illegals.count(key) * value for key, value in points.items())
+
+
+assert get_score(test_input) == 26397
+print(get_score(_input))  # 415953
+
+
+# %%
+def get_score2(data: list) -> int:
+    points = {")": 1, "]": 2, "}": 3, ">": 4}
+    incomplete = tuple(
+        filter(lambda x: isinstance(x, list), [balanced_paren(test) for test in data])
+    )
+
+    def calculate_score(line: List[str]) -> int:
+        score = 0
+        for ket in line:
+            score = score * 5 + points[ket]
+        return score
+
+    ##return sum(illegals.count(key) * value for key, value in points.items())
+    scores = [calculate_score(l) for l in incomplete]
+    return sorted(scores)[len(scores) // 2]
+
+
+assert get_score2(test_input) == 288957
+assert get_score2(_input) == 2292863731
+
+# %%
