ed9fa7fbd156fd2d7248261885c1660e68ce939a
Nehemiah Campbell <nehemiahcampbell@hotmail.co.uk>
2021-12-10 08:44:00 +0000

    2021 day 10 in progress

--- 2021/day10.py
+++ 2021/day10.py
@@ -1,3 +1,4 @@
+from typing import Union, List
 from advent_of_code.core import data
 
 _test_input = """[({(<(())[]>[[{[]{<()<>>
@@ -16,7 +17,7 @@ brackets = {"{":"}", "[":"]", "(":")", "<":">"}
 test_input = data(_test_input)
 _input = data('data/input10.txt', test=False)
 
-def balanced_paren(string: str) -> str:
+def balanced_paren(string: str) -> Union[str, List[str]]:
     stack = []
     for char in string:
         if char in brackets:
@@ -25,13 +26,15 @@ def balanced_paren(string: str) -> str:
             stack.pop()
         else:
             return char
-    return ""
+    return stack[::-1]
 
-def get_score(data: list) -> int:
-    points  = {")": 3, "]": 57, "}": 1197, ">": 25137}
-    illegals = [balanced_paren(test) for test in data]
-    return sum(illegals.count(key)*value for key,value in points.items())
+print([balanced_paren(t) for t in test_input])
 
-assert get_score(test_input) == 26397
-print(get_score(_input)) # 415953
+# def get_score(data: list) -> int:
+#     points  = {")": 3, "]": 57, "}": 1197, ">": 25137}
+#     illegals = [balanced_paren(test) for test in data]
+#     return sum(illegals.count(key)*value for key,value in points.items())
+# 
+# assert get_score(test_input) == 26397
+# print(get_score(_input)) # 415953
 
