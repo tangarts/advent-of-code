6b1d917993c99f54c710504846b23323af6413b5
Nehemiah Campbell <nehemiahcampbell@hotmail.co.uk>
2021-12-21 20:19:51 +0000

    2021 day 21 part 2 and cleanup

--- 2021/day21.py
+++ 2021/day21.py
@@ -1,51 +1,37 @@
-#%%
 from functools import lru_cache
 from itertools import product
-from advent_of_code.core import parse_input, integers
+from advent_of_code.core import parse_input
 
 raw = """Player 1 starting position: 4
 Player 2 starting position: 8"""
 
-test = dict(parse_input(raw, parser=integers))
-test
-
+test = parse_input(raw, parser=lambda s: int(s[-1]))
+positions = parse_input('data/input21.txt', parser=lambda s: int(s[-1]), test=False)
 
 def turn(position, roll):
     return (position + roll) % 10
 
-
-assert turn(7, 5) == 2
-assert turn(4, 1 + 2 + 3) == 0
-assert turn(10, 7 + 8 + 9) == 4
-
-
-def part1(player):
+def part1(position):
     # player, starting_pos
-    score = {1: 0, 2: 0}
-
+    score = [0, 0]
     roll = 0
     j = 1
     while True:
-        for i in [1, 2]:
+        for i in [0, 1]:
             roll += 3
-            player[i] = turn(player[i], 3 * (j + 1))
+            position[i] = turn(position[i], 3 * (j + 1))
             j = (j + 3) % 100
-            score[i] += player[i] if player[i] != 0 else 10
+            score[i] += position[i] if position[i] != 0 else 10
             # when statement
             if score[i] > 999:
-                return min(score[1], score[2]) * roll
+                return min(score) * roll
 
 
-print(part1(test))
+# part1
+assert part1(positions) == 926610
 
-#%%
 
-# splits into 3 universes
-# BUT there are only 7 different values for each roll
-# 3..9 calculated from rolls (1,1,1) ... (3,3,3)
-states = {3: 1, 4: 3, 5: 6, 6: 7, 7: 6, 8: 3, 9: 1}
 all_states = [sum(x) for x in product([1, 2, 3], repeat=3)]
-#%%
 @lru_cache(maxsize=None)
 def play(position1, position2, score1, score2):
 
@@ -64,12 +50,5 @@ def play(position1, position2, score1, score2):
         win2 += w2
     return win1, win2
 
-
-#%%
-
-
-# %%
-play(4, 8, 0, 0)
-
-# %%
-max(play(2, 6, 0, 0))  # 273042027784929
+# part2
+assert max(play(6, 2, 0, 0)) == 146854918035875
